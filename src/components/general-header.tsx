1506ecaf5f753a3f9df9bbc2f8534681c68db151
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-06-12 12:57:53 +0900

    fix: adjust z-index values for header and loader components for proper stacking context

--- src/components/general-header.tsx
+++ src/components/general-header.tsx
@@ -2,7 +2,7 @@ export default function GeneralHeader({
   children,
 }: { children?: React.ReactNode }) {
   return (
-    <header className='bg-white border-b'>
+    <header className='bg-white border-b z-20'>
       <div className='mx-auto flex h-18 max-w-[1340px] items-center justify-between gap-8 px-3 sm:px-6 lg:px-6'>
         {/* <div className='flex flex-1 items-center justify-end md:justify-between'> */}
         {/* <nav aria-label='Global' className='hidden md:block'>
